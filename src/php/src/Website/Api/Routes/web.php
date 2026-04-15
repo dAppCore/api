5f8347957162615bea8dc72f6e4572a9ec3aca8e
Snider <snider@host.uk.com>
2026-04-16 00:31:08 +0100

    Align API reference route with RFC

--- src/php/src/Website/Api/Routes/web.php
+++ src/php/src/Website/Api/Routes/web.php
@@ -21,7 +21,7 @@ Route::get('/changelog', [DocsController::class, 'changelog'])->name('api.change
 
 // API Reference
 Route::get('/reference', [DocsController::class, 'reference'])->name('api.reference');
-Route::get('/api/reference', [DocsController::class, 'reference'])->name('api.reference.compat');
+Route::get('/api/reference', [DocsController::class, 'redoc'])->name('api.reference.compat');
 Route::get('/docs/api', [DocsController::class, 'swagger'])->name('api.docs.api');
 Route::get('/openapi.yaml', [DocsController::class, 'openapiYaml'])
     ->middleware('throttle:60,1')
