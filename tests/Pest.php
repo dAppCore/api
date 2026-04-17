67b52c970576248aa125dc924644323776516af7
Snider <snider@host.uk.com>
2026-04-17 16:49:28 +0100

    Fix Pest bootstrap provider signature

--- tests/Pest.php
+++ tests/Pest.php
@@ -32,7 +32,7 @@ $_SERVER['argv'][1] = $_SERVER['argv'][1] ?? 'route:list';
 
 abstract class TestCase extends Orchestra\Testbench\TestCase
 {
-    protected function getPackageProviders(\Illuminate\Foundation\Application $app): array
+    protected function getPackageProviders($app): array
     {
         return [
             Core\Api\Boot::class,
